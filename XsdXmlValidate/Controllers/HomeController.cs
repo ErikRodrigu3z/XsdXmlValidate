306203c63ef275b1a97046fb6df19c81df9c75d4
erik <erik.rodrigu3z@gmail.com>
2020-08-08 04:09:12 -0600

    Add project files.

---
+++ XsdXmlValidate/Controllers/HomeController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using XsdXmlValidate.Helper;
+
+namespace XsdXmlValidate.Controllers
+{
+    public class HomeController : Controller
+    {
+        Helper.Validations val = new Validations();
+        public ActionResult Index()
+        {
+            
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(HttpPostedFileBase file)
+        {
+            if (file.ContentLength > 0)
+            {
+                var fileName = file.FileName;
+                var path = Path.Combine(Server.MapPath("~/uploads"), fileName.ToString());
+                file.SaveAs(path);
+
+                //localidad el archivo xsd
+                string xsd = Server.MapPath("~/Xsd/input.xsd");
+                ViewBag.Mensaje =  val.ValidateXmlUsingXSD(xsd, Server.MapPath("~/uploads/" + file.FileName));
+
+
+                
+            }
+
+            return View();
+        }
+
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ XsdXmlValidate/Helper/Validations.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Security.Policy;
+using System.Web;
+using System.Web.Mvc;
+using System.Xml;
+
+namespace XsdXmlValidate.Helper
+{
+    public class Validations
+    {
+        public string ValidateXmlUsingXSD(string xsd,string xml)
+        { 
+            try
+            {
+
+                XmlReaderSettings settings = new XmlReaderSettings();
+                settings.ValidationType = ValidationType.Schema;
+                settings.Schemas.Add(null, XmlReader.Create(xsd)); 
+
+                XmlReader xmlReader = XmlReader.Create(xml, settings);
+                XmlDocument xmlDoc = new XmlDocument();
+                xmlDoc.Load(xmlReader);
+
+                return "Success";
+            }
+            catch (Exception ex)
+            {
+
+                return ex.Message;
+            }
+        } 
+
+
+    }
+}
